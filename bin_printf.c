ee853cc6df20c21388a760313c8afa48a0c9ec77
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-25 17:49:41 -0700

    print a binary

---
+++ bin_printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * printf_bin - Function
+ *
+ * Description: Convert an unsigned integer into binary.
+ *
+ * @num: Unsigned integer to convert into binary.
+ * @output: String to append the result to.
+ *
+ * Return: The length to append to output.
+ */
+int printf_bin(int num, char *output)
+{
+	char *bin = malloc(32);
+	int i = 0, j = 0, length;
+	char temp;
+
+	if (num == 0)
+	{
+		_strcpy("0", output);
+		return (0);
+	}
+
+	while (num > 0)
+	{
+		bin[i] = (num % 2) + '0';
+		num /= 2;
+		i++;
+	}
+	bin[i] = '\0';
+	j = i - 1;
+	i = 0;
+	while (i < j)
+	{
+		temp = bin[i];
+		bin[i] = bin[j];
+		bin[j] = temp;
+		i++;
+		j--;
+	}
+	_strcpy(bin, output);
+	length = _strlen(bin) - 1;
+	free(bin);
+	return (length);
+
+}
